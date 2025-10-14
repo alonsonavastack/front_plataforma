433999df14a06ed7462c7312cfec2ad828774cc9
Alonso Nava <alonso.nava086@gmail.com>
2025-10-14 16:03:37 -0600

    ultima

---
+++ src/app/pages/configuracion-fiscaly-pago/configuracion-fiscaly-pago.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-configuracion-fiscaly-pago',
+  imports: [],
+  templateUrl: './configuracion-fiscaly-pago.html',
+  styleUrl: './configuracion-fiscaly-pago.css'
+})
+export class ConfiguracionFiscalyPago {
+
+}
