0b7b2a999b6e986083ed64c0c7eddf2f94a478d0
Alonso Nava <alonso.nava086@gmail.com>
2026-02-04 22:48:04 -0600

    sisi1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  url: 'https://api.devhubsharks.com',  // ← Cambiar aquí
+  url: 'https://devhubsharks.com/api',  // ← Cambiar aquí (sin "api." al inicio)
   images: {
-    user: 'https://api.devhubsharks.com/users/imagen-usuario/',
-    cat: 'https://api.devhubsharks.com/categories/imagen-categorie/',
-    course: 'https://api.devhubsharks.com/courses/imagen-course/',
-    project: 'https://api.devhubsharks.com/projects/imagen-project/',
+    user: 'https://devhubsharks.com/api/users/imagen-usuario/',
+    cat: 'https://devhubsharks.com/api/categories/imagen-categorie/',
+    course: 'https://devhubsharks.com/api/courses/imagen-course/',
+    project: 'https://devhubsharks.com/api/projects/imagen-project/',
   },
   paypal: {
     clientId: 'AZc1SmomD67615PERyjzwXXf6wO02x7SwjKjPde5J8TycUyhar3nNTePaoR6Mvd-t2tzjnKD9ji7hc1w',
